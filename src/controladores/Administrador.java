fea1a36814d212b3038b58f08c24d53960b65c8f
Franco <francomassii1@gmail.com>
2023-11-09 16:59:19 -0300

    VERSION 1.4

--- src/controladores/Administrador.java
+++ src/controladores/Administrador.java
@@ -7,6 +7,7 @@ public class Administrador extends Usuario {
 	
 	private ArrayList<Sede> sedes_asignadas;
 	private ArrayList<Clase> clases_a_administrar;
+	private Grabaciones grabaciones;
 
 	public Administrador(String correo_electronico, String contraseña) {
 		super(correo_electronico, contraseña);
@@ -23,7 +24,50 @@ public class Administrador extends Usuario {
 	public void agregarAccesoAClase(Clase clase) {
 		clases_a_administrar.add(clase);
 	}
+
+	public void setAccesoAGrabaciones(Grabaciones clases_grabadas) {
+		this.grabaciones = clases_grabadas;
+	}
 	
+	public void crearNuevaClase(Profesor profesor, Emplazamiento emplazamiento, Sede sede, Disciplina disciplina, String horario, String dia) {
+    	Clase clase = new Clase(profesor, sede, emplazamiento, disciplina, dia, horario);
+    	clases_a_administrar.add(clase);
+		if (clase.getDisciplina().getVirtualidad()) {
+            grabaciones.agregarClase(clase);
+        }
+    }
+
+	//METODO PARA MOSTRAR CLASES GRABADAS
+	public void verClasesGrabadaas(Grabaciones grabaciones) {
+		grabaciones.mostrarGrabaciones();
+	}
+
+	//METODO PARA ELIMINAR CLASES GRABADAS
+	public void eliminarClases(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese la cantidad de clases de Yoga y Gimnasia que quiere borrar" +
+															"o '0' si no quiere borrar ninguna: ");
+		String cant_clases_a_borrar = scanner.nextLine();
+		try {
+			int clases_a_borrar = Integer.parseInt(cant_clases_a_borrar);
+			grabaciones.eliminarClases(clases_a_borrar);
+		} catch (Exception e) {
+			System.out.println("INGRESO NO VALIDO");
+		}
+	}
+
+	//METODO PARA GESTIONAR LAS GRABACIONES DE CLASES
+	public void gestionarGrabaciones() {
+		verClasesGrabadaas(grabaciones);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Desea eliminar algunas grabaciones? (S/N): ");
+		String borrar = scanner.nextLine();
+		if (borrar.toUpperCase() == "S") {
+			eliminarClases();
+		}
+	}
+
+
 	//METODO PARA MONITORIEAR LA CANTIDAD DE ARTICULOS EN UNA SEDE Y SU ESTADO DE DESGASTE
 	public void mostrarArticulosSede(Sede sede) {
 		HashMap<Articulo, Integer> articulos_de_sede = sede.getCantidadStock();
@@ -33,8 +77,7 @@ public class Administrador extends Usuario {
             		+ ", Cantidad disponible: " + parCV.getValue());
         }
 	}
-	
-	
+
 	//METODO PARA AGREGAR MODIFICAR LA CANTIDAD DE ARTICULOS DE UNA SEDE
     public void agregarArticulos(Sede sede, Articulo articulo, int cantidad) {
     	sede.agregar_articulo(articulo, cantidad);
--- src/sistema/Grabaciones.java
+++ src/sistema/Grabaciones.java
@@ -4,14 +4,36 @@ import java.util.*;
 
 public class Grabaciones {
 	
-	private ArrayList<Clase> clases_grabadas;
+	private ArrayList<Clase> yoga_virtual = new ArrayList<>();
+	private ArrayList<Clase> gimnasia_virtual = new ArrayList<>();
 	
 	public void agregarClase(Clase clase){
-		clases_grabadas.add(clase);
-		if (clases_grabadas.size() > 10) {
-			clases_grabadas.remove(0);
+		if (clase.getDisciplina().getNombre() == "YOGA") {
+			if (yoga_virtual.size() >= 10) {
+				yoga_virtual.remove(0);
+				}
+			yoga_virtual.add(clase);
+			} else if (clase.getDisciplina().getNombre() == "GIMNASIA POSTURAL") {
+			if (gimnasia_virtual.size() >= 15) {
+				gimnasia_virtual.remove(0);
+				} 
+			gimnasia_virtual.add(clase);
+			}
+		}
+
+	public void mostrarGrabaciones() {
+		for (Clase clase: yoga_virtual) {
+			System.out.println("Clase: " + clase.getDisciplina().getNombre() + ", Dia" + clase.getDia() + ", " + clase.getHorario());
+		}
+		for (Clase clase: gimnasia_virtual) {
+			System.out.println("Clase: " + clase.getDisciplina().getNombre() + ", Dia" + clase.getDia() + ", " + clase.getHorario());
 		}
 	}
 
-	
-}
+	public void eliminarClases(int cantidad_clases_a_eliminar) {
+		for (int i = 0; i > cantidad_clases_a_eliminar; i++) {
+			yoga_virtual.remove(i);
+			gimnasia_virtual.remove(i);
+		}
+	}
+	}
